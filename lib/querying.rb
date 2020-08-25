1a150c797308e7de3bee685ab4e656f1a557e3d5
Micah Bowie <micahbowie20@gmail.com>
2020-08-25 02:58:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,8 @@ def select_series_title_with_most_human_characters
       WHERE characters.species = 'human'
       GROUP BY characters.series_id
       ORDER BY num_species DESC
-      LIMIT 1);"
+      LIMIT 1 
+      );"
 end
 
 def select_character_names_and_number_of_books_they_are_in
